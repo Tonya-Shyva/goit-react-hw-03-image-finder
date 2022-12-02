ae40c049f1b580402dca7bc132ce0849372c6442
Tonya-Shyva <toshash69@gmail.com>
2022-12-02 21:04:57 +0200

    feature/axios.get

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,28 @@
 import { Component } from 'react';
+import axios from 'axios';
 
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 import Modal from 'components/Modal/Modal';
 import SearchBar from 'components/Searchbar/Searchbar';
 import AppContainer from './App.styled';
 
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '30150514-c6c2592e7290a81c416aa6291';
+// let trimmedValue = 'cat';
+// let pageNumber = 1;
+
 export class App extends Component {
   state = {
+    images: [],
     shownModal: false,
   };
+
+  async componentDidMount(/*trimmedValue, pageNumber*/) {
+    const response = await axios.get(
+      `/?key=${API_KEY}&q="cat"&orientation=horizontal&safesearch=true&image_type=photo&per_page=12&page=1`
+    );
+    this.setState({ images: response.data.hits });
+  }
 
   toggleModal = () => {
     this.setState(({ showModal }) => ({
